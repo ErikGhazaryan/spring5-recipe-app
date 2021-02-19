a603298e4471d9f406a14a21af8c1d95bf9056f4
Erik Ghazaryan <erikghazarian@gmail.com>
2021-02-19 12:55:33 +0100

    committed

--- src/main/java/guru/springframework/exceptions/NotFoundException.java
+++ src/main/java/guru/springframework/exceptions/NotFoundException.java
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-
+	String messageTrial = "this is my message for 404"; 
     public NotFoundException() {
         super();
     }
 
     public NotFoundException(String message) {
-        super(message);
+        super("this is my message for 404");
     }
 
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public NotFoundException(String messageTrial, Throwable cause) {
+        super(messageTrial, cause);
     }
 
 }
